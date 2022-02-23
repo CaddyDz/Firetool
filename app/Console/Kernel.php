f93aebd4cb289c67a5e928da9083ee6a2fe2ffac
Salim Djerbouh <caddydz4@gmail.com>
2022-02-23 20:29:08 +0100

    Register commands
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,5 +8,15 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-	//
+	/**
+	 * Register the commands for the application.
+	 *
+	 * @return void
+	 */
+	protected function commands(): void
+	{
+		$this->load(__DIR__ . '/Commands');
+
+		require base_path('routes/console.php');
+	}
 }
